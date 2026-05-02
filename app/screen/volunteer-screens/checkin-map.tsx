074382e80e6d0e767b74317c33cdf0cc0cea771a
ANHBHN3 <anhbhn3@fpt.com>
2026-05-02 14:03:27 +0700

    fix: resolve missing event session id in quick checkin

--- app/screen/volunteer-screens/checkin-map.tsx
+++ app/screen/volunteer-screens/checkin-map.tsx
@@ -98,6 +98,7 @@ const CheckinMapScreen = () => {
 
             // 3. Call quickCheckIn API to create the CheckInLog on the BE
             await quickCheckInApi({
+                eventSessionId: params.eventSessionId,
                 applicationId: params.applicationId,
                 deviceId,
                 apVersion,
